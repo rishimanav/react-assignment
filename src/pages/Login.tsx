a278bba6c57e48a4e637eb200a3db7db4469eb50
Rishi Jain <rishimanav23@gmail.com>
2023-06-16 12:55:20 +0530

    Styled the login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -23,52 +23,59 @@ export default class Login extends React.Component<LoginProps> {
 
   render() {
     return (
-      <div className="login-page row justify-content-center bg-primary">
+      <div className="login-page row justify-content-center">
       {/* <!-- fixed the size of the form to 9 columns --> */}
-      <div className="col-8">
-        <div className="custom-form">
-          <form>
+      <div className="col-7">
+        <div className="card bg-primary rounded shadow">
+          <div className="card-header border-0 text-white fw-bolder login-header">
+            Login
+          </div>
+          <div className="card-body">
+            <div className="custom-form">
+              <form>
 
-            {/* <!-- username input --> */}
-            <div className="row">
-              <div className="col-12 mb-3">
-                <label htmlFor="username" className="form-label text-white fw-bold">Username* :</label>
-                <input
-                  name="username"
-                  id="username_input"
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter Name"
-           
-                  />
-              </div>
-            </div>
-
-            {/* <!-- password input --> */}
-            <div className="row">
-              <div className="col-l2">
-                <div className="mb-3">
-                  <label htmlFor="password" className="form-label text-white fw-bold">Password*:</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    name="password"
-                    id="password"
-                    placeholder="Password"/>
+                {/* <!-- username input --> */}
+                <div className="row">
+                  <div className="col-12 mb-3">
+                    <label htmlFor="username" className="form-label text-white label-text ">Username*</label>
+                    <input
+                      name="username"
+                      id="username_input"
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter Name"
+              
+                      />
+                  </div>
                 </div>
-              </div>
-            </div>
 
-            {/* Submit Button */}
-            <div className="mt-3 text-end">
-              <Link to="/home">
-                <button className="btn btn-danger rounded-pill fw-bold" 
-                onClick={()=>this.pushNameChanges()}>Submit</button>
-              </Link>
-            </div>
+                {/* <!-- password input --> */}
+                <div className="row">
+                  <div className="col-l2">
+                    <div className="mb-3">
+                      <label htmlFor="password" className="form-label text-white label-text">Password*</label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        name="password"
+                        id="password"
+                        placeholder="Password"/>
+                    </div>
+                  </div>
+                </div>
 
-          </form>
-          
+                {/* Submit Button */}
+                <div className="mt-3 text-end">
+                  <Link to="/home">
+                    <button className="btn btn-danger rounded-pill login-button" 
+                    onClick={()=>this.pushNameChanges()}>Submit</button>
+                  </Link>
+                </div>
+
+              </form>
+              
+            </div>
+          </div>
         </div>
       </div>
       </div>
